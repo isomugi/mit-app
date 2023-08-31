4c45838c34fea5e771ef370f5c59c56a3691c41f
EC2 Default User <ec2-user@ip-172-31-5-171.ap-northeast-1.compute.internal>
2023-08-31 05:26:22 +0000

    rename

--- utils.sh
+++ utils.sh
@@ -50,7 +50,7 @@ lambda_deploy() {
         zip -r ${funame}.zip ${filename}
         aws lambda create-function  --function-name ${funame} \
         --runtime nodejs18.x \
-        --role arn:aws:iam::347867041416:role/internship_exec_role \
+        --role arn:aws:iam::387258525885:role/cloud9-mti-role \
         --handler ${funame}.handler  --zip-file fileb://${funame}.zip \
         --region ap-northeast-1
         rm ${funame}.zip
